c4046da1214e2fc52f1796e4afc897361a260044
Sascco <sascco2020@gmail.com>
2024-06-25 20:54:02 -0500

    Prueba vino

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
-urban_routes_url = 'https://cnt-c2d71d4c-b289-4b4d-ac8f-b7c46f968461.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-1b87312f-e6ec-4269-a2a7-637b5cee0527.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
 card_number, card_code = '1234 5678 9100', '111'
-message_for_driver = 'trae 2 botellas de agua'
+message_for_driver = 'trae 2 botellas de vino'
 
 
