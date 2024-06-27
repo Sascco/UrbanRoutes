4202864fcf48727da556775b3e2de8c487ac3b68
Sascco <sascco2020@gmail.com>
2024-06-26 21:00:34 -0500

    Test 1,2 Passed
    Se actualizaron las variables en CAPS=constantes en los tests.

--- main.py
+++ main.py
@@ -21,26 +21,26 @@ class TestUrbanRoutes:
         chrome_options.add_experimental_option("perfLoggingPrefs", {'enableNetwork': True, 'enablePage': True})
         chrome_options.set_capability("goog:loggingPrefs", {'performance': 'ALL'})
         cls.driver = webdriver.Chrome()  # desired_capabilities = capabilities
-        cls.driver.get(data.urban_routes_url)
+        cls.driver.get(data.URBAN_ROUTES_URL)
         cls.driver.implicitly_wait(10)
 
     #prueba 1 - Configurar la dirección (esta parte se ha escrito para ti como ejemplo).
     def test_set_route(self):
-        self.driver.get(data.urban_routes_url)
+        self.driver.get(data.URBAN_ROUTES_URL)
         set_route = UrbanRoutesPage(self.driver)
-        set_route.set_from(data.address_from)
-        set_route.set_to(data.address_to)
+        set_route.set_from(data.ADDRESS_FROM)
+        set_route.set_to(data.ADDRESS_TO)
         from_1 = set_route.get_from()
-        assert from_1 == data.address_from
+        assert from_1 == data.ADDRESS_FROM
         set_route.click_book_a_taxi_button()
         sleep(2)
 
     #prueba 2
     def test_comfort_fare(self):
-        self.driver.get(data.urban_routes_url)
+        self.driver.get(data.URBAN_ROUTES_URL)
         set_route = UrbanRoutesPage(self.driver)
-        set_route.set_from(data.address_from)
-        set_route.set_to(data.address_to)
+        set_route.set_from(data.ADDRESS_FROM)
+        set_route.set_to(data.ADDRESS_TO)
         set_route.click_book_a_taxi_button()
         set_route.comfort_fare_button()
         # assert set_route.comfort_fare_button()    Fix this assert!!!!
@@ -48,10 +48,11 @@ class TestUrbanRoutes:
 
 
     # def test_fill_phone_number_box(self):  # Rellenar el número de teléfono.
-    #     self.driver.get(data.urban_routes_url)
+    #     self.driver.get(data.URBAN_ROUTES_URL)
     #     set_route = UrbanRoutesPage(self.driver)
-    #     set_route.set_from(data.address_from)
-    #     set_route.set_to(data.address_to)
+    #     set_route.set_from(data.ADDRESS_FROM)
+    #     set_route.set_to(data.ADDRESS_TO)
+    #     set_route.click_book_a_taxi_button()
     #     set_route.phone_popup_window_input_box(data.phone_number)
     #     set_route.phone_popup_window_input_box_next()
     #     set_route.code_field()
@@ -60,10 +61,10 @@ class TestUrbanRoutes:
 
     #
     def test_add_payment_method(self):  # Agregar tarjeta de credito como metodo de pago
-        self.driver.get(data.urban_routes_url)
+        self.driver.get(data.URBAN_ROUTES_URL)
         set_route = UrbanRoutesPage(self.driver)
-        set_route.set_from(data.address_from)
-        set_route.set_to(data.address_to)
+        set_route.set_from(data.ADDRESS_FROM)
+        set_route.set_to(data.ADDRESS_TO)
         set_route.click_book_a_taxi_button()
         sleep(2)
         set_route.add_new_credit_card()
@@ -75,10 +76,10 @@ class TestUrbanRoutes:
 
     #prueba 5 Escribir un mensaje para el controlador.
     # def test_special_instruction_for_service(self): # Mensaje para conductor
-    #     self.driver.get(data.urban_routes_url)
+    #     self.driver.get(data.URBAN_ROUTES_URL)
     #     set_route = UrbanRoutesPage(self.driver)
-    #     set_route.set_from(data.address_from)
-    #     set_route.set_to(data.address_to)
+    #     set_route.set_from(data.ADDRESS_FROM)
+    #     set_route.set_to(data.ADDRESS_TO)
     #     set_route.click_book_a_taxi_button()
     #     sleep(2)
     #     set_route.comfort_fare_button()
@@ -90,20 +91,20 @@ class TestUrbanRoutes:
 
 
     # def test_ordering_blanket(self): #Pedir una manta y pañuelos.
-    #     self.driver.get(data.urban_routes_url)
+    #     self.driver.get(data.URBAN_ROUTES_URL)
     #     set_route = UrbanRoutesPage(self.driver)
-    #     set_route.set_from(data.address_from)
-    #     set_route.set_to(data.address_to)
+    #     set_route.set_from(data.ADDRESS_FROM)
+    #     set_route.set_to(data.ADDRESS_TO)
     #     set_route.click_book_a_taxi_button()
     #     set_route.comfort_fare_button()
     #     sleep(3)
     #     set_route.blanket_and_scarves()
 
     # def test_add_icecream(self): # Pedir 2 helados.
-    #     self.driver.get(data.urban_routes_url)
+    #     self.driver.get(data.URBAN_ROUTES_URL)
     #     set_route = UrbanRoutesPage(self.driver)
-    #     set_route.set_from(data.address_from)
-    #     set_route.set_to(data.address_to)
+    #     set_route.set_from(data.ADDRESS_FROM)
+    #     set_route.set_to(data.ADDRESS_TO)
     #     set_route.click_book_a_taxi_button()
     #     sleep(3)
     #     set_route = UrbanRoutesPage(self.driver)
