6a14dd417d509a7708cc337fd6f736eb2d48f7d7
Sascco <sascco2020@gmail.com>
2024-06-29 10:43:08 -0500

    Test 1,2,3,4,5,6,7 Passed
    Falta la prueba 8

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-URBAN_ROUTES_URL = 'https://cnt-db7649a1-030a-47d7-b0b9-dd5bdc072ab7.containerhub.tripleten-services.com?lng=es'
+URBAN_ROUTES_URL = 'https://cnt-130fbd21-a3d6-4a66-9b81-bfb38a871362.containerhub.tripleten-services.com?lng=es'
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
